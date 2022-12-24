d2e5d8f9b7832637cde793b519b33bf41f8845e8
Artem Khomenko <artem011202@gmail.com>
2022-12-23 18:00:26 -0800

    Read me

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -9,7 +9,7 @@ export default function Contact() {
      <section id="Contact">
             <h2>Contact Me</h2>
       
-                <h3 className="contact"><i className="fa-solid fa-envelope"></i> <a href="mailto:artem011202@gmail.com">Email Me</a></h3>
+                <h3 className="contact"><i className="fa-solid fa-envelope"></i> <a href="mailto:artem011202@gmail.com">My Email</a></h3>
                 <h3 className="contact">
                     Feel free to send me an email.
                 </h3>
